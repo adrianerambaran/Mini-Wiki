078a4aa925583cf8e0d7fcac8467cc5e3de56cbb
Adriane Rambaran <adrianerambaran@csus.edu>
2017-08-23 17:49:49 -0700

    Update CustomError.js

--- CustomError.js
+++ CustomError.js
@@ -1,4 +1,7 @@
-
+/*
+-This extends Javascripts generic Error class. 
+-Give a custom error code for error handling.
+*/
 module.exports = function customError(mes, code)
 {
 	this.Error = mes || 'Error';
